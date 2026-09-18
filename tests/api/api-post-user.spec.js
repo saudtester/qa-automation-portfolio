d012cd3b1bef6026a41f2e1c931bf98bd44fd420
Saud Malik <saud.nosherwan@gmail.com>
2026-09-18 15:02:57 +0500

    Strengthen API resposne and negative testing

--- tests/api/api-post-user.spec.js
+++ tests/api/api-post-user.spec.js
@@ -18,13 +18,52 @@ test('POST creates a user', async({apiContext})=>{
 
     expect(response.status()).toBe(201);
 
+    const headers = response.headers();
+
+    expect(headers).toHaveProperty('content-type');
+
+    expect(headers['content-type']).toContain('application/json');
+
     const body = await response.json();
+
+    expect(body).toHaveProperty('name');
+    
+    expect(body).toHaveProperty('job');
+    
+    expect(body).toHaveProperty('id');
+    
+    expect(body).toHaveProperty('createdAt');
 
     expect(body.name).toBe('Saud');
 
     expect(body.job).toBe('QA Engineer');
 
-    expect(body.id).toBeTruthy();
+    expect (typeof body.id).toBe('string');
+
+    expect(body.id).not.toBe('');
+
+    expect (typeof body.createdAt).toBe('string');
+
+    expect(body.createdAt).not.toBe('');
 
 });
 
+test('POST login fails without password', async({apiContext})=>{
+    const response = await apiContext.post('https://reqres.in/api/login',
+        {
+            data:
+            {
+                email: 'abdcddf@fsdfjk.com'
+            }
+        }
+    );
+
+    expect(response.status()).toBe(400);
+
+    const body = await response.json();
+
+    expect(body).toHaveProperty('error');
+
+    expect(body.error).toBe('Missing password');
+
+});
